270904ceb5fc3180a08eff62bfca9a1f1c01222f
arieftb22 <arieftb22@gmail.com>
2020-07-20 10:50:10 +0700

    add post voters by event service

--- app/Http/Controllers/VoterController.php
+++ app/Http/Controllers/VoterController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Credentials;
+use App\Models\Events;
+use App\Models\Voters;
+use Illuminate\Http\Request;
+
 class VoterController extends BaseController
 {
     /**
@@ -16,8 +21,31 @@ class VoterController extends BaseController
 
     //
 
-    public function storeByEvent($request)
+    public function storeByEvent(Request $request, $event_id)
     {
-        
+        $user_id = Credentials::where(CREDENTIAL_TOKEN_FIELD, $request->header(HEADER_AUTH_KEY))->firstOrFail()->user_id;
+        $event_id_data = Events::where(ID_FIELD, $event_id)->where(EVENT_CODE_FIELD, $request->input(EVENT_CODE_FIELD))->firstOrFail()->id;
+
+
+        if ($user_id && $event_id_data) {
+            $request[USER_ID_FOREIGN_FIELD]  = $user_id;
+            $request[EVENT_ID_FOREIGN_FIELD] = $event_id;
+
+            try {
+                //code...
+                $voter = Voters::create($request->all());
+
+                if ($voter) {
+                    return $this->response(null, 201);
+                } else {
+                    return $this->response(null, 400);
+                }
+            } catch (\Throwable $th) {
+                //throw $th;
+                return $this->responseError($th->getCode(), $th->getMessage());
+            }
+        } else {
+            return $this->response(null, 400);
+        }
     }
 }
