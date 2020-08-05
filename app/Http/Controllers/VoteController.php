311d34135c28262c483056ecb3d81471566a95cb
arieftb22 <arieftb22@gmail.com>
2020-08-05 18:18:38 +0700

    add post vote service

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -2,12 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Candidates;
+use App\Models\Credentials;
+use App\Models\Voters;
+use App\Models\Votes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class VoteController extends BaseController
 {
     public function store(Request $request, $event_id, $id)
     {
-        
+        $credential = Credentials::where(CREDENTIAL_TOKEN_FIELD, $request->header(HEADER_AUTH_KEY))->first();
+        $userId = $credential != null ? $credential->user_id : null;
+
+        $voter = Voters::where(EVENT_ID_FOREIGN_FIELD, $event_id)->where(USER_ID_FOREIGN_FIELD, $userId)->first();
+        $event = $voter->events;
+        if ($event->id != $event_id) return $this->response(null, 401);
+        if (!$voter) return $this->response($voter, 404);
+        if ($voter->is_active != 1) return $this->response(null, 401);
+        if ($voter->votes) return $this->response(null, 409);
+
+        $candidate = Candidates::find($id);
+        if (!$candidate) return $this->response($candidate, 404);
+
+        $request[CANDIDATE_ID_FOREIGN_FIELD] = $candidate->id;
+        $request[VOTER_ID_FOREIGN_FIELD] = $voter->id;
+
+        $validationRequest = Validator::make($request->all(), Votes::voteRule());
+        if ($validationRequest->fails()) return $this->response(null, 400, MESSAGE_ERROR_CANDIDATE_FAILED_POST);
+
+        try {
+            $vote = Votes::create($request->all());
+            if ($vote) {
+                return $this->response(null, 201);
+            }        
+            return $this->response(null, 400);
+        } catch (\Throwable $th) {
+            return $this->responseError($th->getCode(), $th->getMessage());
+        }
     }
 }
--- app/Models/Votes.php
+++ app/Models/Votes.php
@@ -10,6 +10,14 @@ class Votes extends BaseModel
 
     protected $guarded = [VOTE_ID_FIELD];
 
+    public static function voteRule()
+    {
+        return [
+            VOTER_ID_FOREIGN_FIELD => 'required|numeric',
+            CANDIDATE_ID_FOREIGN_FIELD => 'required|numeric'
+        ];
+    }
+
     public function voters()
     {
         return $this->belongsTo(Voters::class, VOTER_ID_FOREIGN_FIELD);
